1d48846a60c67778a0a12b65f8b4ce6660415c54
Ivan Mikhailov <mikhailov.ivan@gmail.com>
2023-10-17 16:27:39 +0300

    initial commit

---
+++ comb_brush_scf/__init__.py
@@ -0,0 +1,2 @@
+from .script_creation import (script_creation, mol_script)
+__all__ = ["script_creation", "mol_script"]
---
+++ comb_brush_scf/script_creation.py
@@ -0,0 +1,63 @@
+
+
+def script_creation(N: int, n: int, m: int, sigma: float) -> None:
+    if N < 3:
+        raise ValueError("N < 3")
+    if n < 0:
+        raise ValueError("n < 0")
+    if m < 1 and m > N//2:
+        raise ValueError("m < 1 and m > N//2")
+    if N % m != 0:
+        raise ValueError("N % m != 0")
+    if sigma < 0:
+        raise ValueError("sigma < 0")
+    if sigma > (1 + n/m)**(-1):
+        raise ValueError("sigma > (1 + n/m)**(-1)")
+
+    theta = (1+N+(N//m-1)*n)*sigma
+
+    with open('brush.in', 'w') as file1:
+        file1.writelines(
+            f"""
+    lat : flat : n_layers : {N+n}
+    lat : flat : geometry : flat
+    lat : flat : lambda : 0.16666666666666666667
+    lat : flat : lowerbound : surface
+    lat : flat : upperbound : surface
+
+    mon : solid : freedom : frozen
+    mon : solid : frozen_range : upperbound
+    mon : W : freedom : free
+    mon : X: freedom : pinned
+    mon : X : pinned_range : 1
+    mon : A : freedom : free
+    
+    mol : solvent : composition : (W)1
+    mol : solvent : freedom : solvent
+
+    mol : pol : composition : {mol_script(N, m, n)}
+    mol : pol : freedom : restricted
+    mol : pol : theta : {theta}
+
+    output : filename.pro : type : profiles
+    output : filename.pro : template : profile.template
+
+    start
+    
+    """
+        )
+
+
+def mol_script(N: int, m: int, n: int) -> str:
+
+    if N <= 0:
+        raise Exception("N1 must be > 0")
+
+    if m < 1:
+        raise Exception("m must be >= 1")
+    if n < 0:
+        raise Exception("m must be >= 0")
+    if n == 0:
+        return f'(X)1(A){N}'
+    else:
+        return f'(X)1((A){m}[(A){n}]){N//m-1}(A){m}'
